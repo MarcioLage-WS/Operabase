a9c72b6c5acb38c77815a930c3428699dbdf51c9
MarcioLage-WS <marcio@ws.com.br>
2026-02-21 09:08:20 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,21 @@
+import { supabase } from './supabaseClient.js'
 
+const form = document.getElementById('loginForm')
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  })
+
+  if (error) {
+    alert('Erro no login: ' + error.message)
+  } else {
+    window.location.href = "dashboard.html"
+  }
+})
